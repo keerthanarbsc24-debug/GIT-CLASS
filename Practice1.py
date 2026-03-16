e000c21a8a0dec9d3c4c4e054be9cf6cebafc494
Keerthana R <keerthanarbsc24@rvu.edu.in>
2026-03-16 16:27:24 +0530

    commit

---
+++ Practice1.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+total = num1 + num2
+difference = num1 - num2
+
+print("Total Combined Value:", total)
+print("Difference:", difference)
