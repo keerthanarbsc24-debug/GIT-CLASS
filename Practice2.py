be8360f59b56e24950d4ed26bb8e1b309801764c
Keerthana R <keerthanarbsc24@rvu.edu.in>
2026-03-16 16:30:27 +0530

    commit

---
+++ Practice2.py
@@ -0,0 +1,7 @@
+a = float(input("Enter first value: "))
+b = float(input("Enter second value: "))
+c = float(input("Enter third value: "))
+
+largest = max(a, b, c)
+
+print("The largest value is:", largest)
